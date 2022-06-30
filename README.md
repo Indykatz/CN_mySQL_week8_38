9cae4fe04d6457235b3db159e2ad4a8b5163952a
Indykatz <cats83@me.com>
2022-06-30 14:02:32 +0100

    1st commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CN_mySQL_week8_38
+
+node src/app.js --add --title "Spiderman" --actor "Andrew Garfield"
---
+++ src/app.js
@@ -0,0 +1,28 @@
+const yargs = require("yargs");
+const { sequelize } = require("./db/connection");
+const { addMovie } = require("./movie/functions");
+
+const app = async (yargsObj) => {
+  try {
+    await sequelize.sync({ alter: true });
+    if (yargsObj.add) {
+      //add something to movie table
+      await addMovie({ title: yargsObj.title, actor: yargsObj.actor });
+      console.log("Sucess")
+    } else if (yargsObj.list) {
+      //list contents of movie table
+    } else if (yargsObj.update) {
+      //update one entry in movie table
+    } else if (yargsObj.delete) {
+      //delete entry from table
+    } else {
+      console.log("Incorrect command");
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await sequelize.close();
+  }
+};
+
+app(yargs.argv);
